f06aa8179c8ad0daf81576c20a09edcb7ec82073
nshukuya <nshukuya@lycorp.co.jp>
2026-02-20 14:35:29 +0900

    課題9

---
+++ task/test.py
@@ -0,0 +1,13 @@
+import health
+
+print(type(health))
+print(health)
+
+height = float(input("身長(m)を入力してください："))
+weight = float(input("体重(kg)を入力してください："))
+
+my_bmi = health.bmi(height, weight)
+my_standard_weight = health.standard_weight(height)
+
+print(f"BMI値：{my_bmi:.1f}")
+print(f"標準体重：{my_standard_weight:.1f}kg")
